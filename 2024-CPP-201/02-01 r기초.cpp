d47790d5204cb074dc9cd1c7aa9bc5173a24a9d6
Chae Yeon Woo <s2316@e-mirim.hs.kr>
2024-03-18 14:32:33 +0900

    namespace

--- 2024-CPP-201/02-01 r기초.cpp
+++ 2024-CPP-201/02-01 r기초.cpp
@@ -1,9 +1,9 @@
 ﻿#include <iostream>
 
-
+using namespace std;		// 더이상 std:: 를 안써도 된다.
 void main(void) {
 	// cout 출력객체
 	// // endl 줄바꿈(개행)
-	// std 이름공간(name space)
-	std::cout << "Hello World" << std:: endl;
+	// std 이름공간(name space) 변수의 중복을 막기 위해서
+	cout << "Hello World" << std:: endl;
 }
