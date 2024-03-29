1da3da959c2341fb65d041c17d2ac048979c7146
Chaeyeonwoo <codusdn0210@gmail.com>
2024-03-29 10:15:53 +0900

    배열과 포인터 그리고 함수

--- 2024-CPP-201/pointer/배열과 포인터와 함수.cpp
+++ 2024-CPP-201/pointer/배열과 포인터와 함수.cpp
@@ -5,7 +5,7 @@ void erase(int *_arr, int _length) {
 	for (int i = 0; i < _length; i++)
 		_arr[i] = 0;
 }
-
+//ㅇ
 int main(void) {
 	int arr[4] = { 10, 20, 30, 40 };
 	int length = sizeof(arr) / sizeof(arr[0]);
