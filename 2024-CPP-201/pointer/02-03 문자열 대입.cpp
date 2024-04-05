cafd78ce0746460a7a1a8fd6456961606f76a323
Chaeyeonwoo <codusdn0210@gmail.com>
2024-04-05 09:33:57 +0900

    주영 문자열 대입(복사) - C 스타일2

---
+++ 2024-CPP-201/pointer/02-03 문자열 대입.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <string.h>
+void main(void) {
+	char str[100] = "Juyoung";
+	char str2[100];
+
+	//// str에 있는 내용을 str2에 복사하기
+	//str2[0] = str[0];
+	//str2[1] = str[1];
+	//str2[2] = str[2];
+	//str2[3] = str[3];
+	//str2[4] = str[4];
+	//str2[5] = str[5];
+	//str2[6] = str[6];
+	//str2[7] = str[7];
+	
+	//printf("%s", str2);
+
+	/*int i = 0;
+	for (; str[i] != '\0'; i++) {
+		str2[i] = str[i];
+	}
+	str2[i] = str[i];*/
+	
+	strcpy(str2, str);
+	printf("%s", str2);
+}
