9ea8256c3314b560a15ac0a75bc29461b0c841dd
Chaeyeonwoo <codusdn0210@gmail.com>
2024-04-08 14:43:50 +0900

    구조체는 default가 public

--- 2024-CPP-201/pointer/03-01 구조체와 클래스.cpp
+++ 2024-CPP-201/pointer/03-01 구조체와 클래스.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 // 구조체는 디폴트가 public이여서 가능함.
+
 struct Student {
 	int hakbun_;
 	string name_;
@@ -10,7 +11,7 @@ struct Student {
 
 void main(void) {
 
-	struct Student juyoung;
+	struct juyoung;
 	juyoung.hakbun_ = 2115;
 	juyoung.name_ = "윤주영";
 }
