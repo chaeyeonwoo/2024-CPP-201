6a2a6b15a484793156d174962abd36431dd1793c
Chaeyeonwoo <codusdn0210@gmail.com>
2024-04-08 14:39:22 +0900

    구조체는 defualt가 public

---
+++ 2024-CPP-201/pointer/03-01 구조체와 클래스.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+// 구조체는 디폴트가 public이여서 가능함.
+struct Student {
+	int hakbun_;
+	string name_;
+};
+
+void main(void) {
+
+	struct Student juyoung;
+	juyoung.hakbun_ = 2115;
+	juyoung.name_ = "윤주영";
+}
