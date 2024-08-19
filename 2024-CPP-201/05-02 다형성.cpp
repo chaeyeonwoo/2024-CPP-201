42e1ad5e8ab72f59487577f3895a904498a621a1
Chaeyeonwoo <codusdn0210@gmail.com>
2024-08-19 13:27:46 +0900

    동적할당 복습

---
+++ 2024-CPP-201/05-02 다형성.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Animal {
+public:
+	void bark() {
+
+	}
+	void sleep() {
+
+	}
+	void eat() {
+
+	}
+private://맴버 변수 (속성) (행위x)
+	string name;
+	unsigned int age;
+};
+
+
+void main(void) {
+	Animal* ani = new Animal();
+}
